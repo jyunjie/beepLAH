7d92e9cf8da84f26634f4dc0e791a9203bd6405c
tajunjie <tjunjie29@gmail.com>
2016-07-19 17:34:48 +0800

    First commit

---
+++ beepLAH/CardsViewController.swift
@@ -0,0 +1,40 @@
+//
+//  CardsViewController.swift
+//  beepLAH
+//
+//  Created by JJ on 19/07/2016.
+//  Copyright © 2016 JJ. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class CardsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func logOut(sender: AnyObject) {
+        try! FIRAuth.auth()!.signOut()
+        User.removeUserUid()
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
